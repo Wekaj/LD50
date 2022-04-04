9e2f39d8f9b36026ccc63f90882e7f7e9107b2a8
Wekaj <wekkaj@gmail.com>
2022-04-04 22:18:52 +1200

    Added unit movement

--- src/LD50/Entities/Entity.cs
+++ src/LD50/Entities/Entity.cs
@@ -8,5 +8,9 @@ namespace LD50.Entities {
         public Texture2D? Texture { get; set; }
         public Vector2 Origin { get; set; }
         public Vector2 Scale { get; set; } = Vector2.One;
+        public SpriteEffects Effects { get; set; }
+
+        public Vector2? TargetPosition { get; set; }
+        public float HopTimer { get; set; }
     }
 }
--- src/LD50/Screens/GameScreen.cs
+++ src/LD50/Screens/GameScreen.cs
@@ -15,6 +15,8 @@ namespace LD50.Screens {
         private readonly Texture2D _unitTexture;
         private readonly SpriteFont _font;
 
+        private readonly Random _random = new();
+
         private readonly List<Level> _levels = new();
         private Level _currentLevel;
 
@@ -22,19 +24,19 @@ namespace LD50.Screens {
             _spriteBatch = spriteBatch;
             _bindings = bindings;
 
-            _unitTexture = content.Load<Texture2D>("Textures/Character Test 1");
+            _unitTexture = content.Load<Texture2D>("Textures/Character Test 3");
             _font = content.Load<SpriteFont>("Fonts/font");
             
-            var random = new Random();
             for (int i = 0; i < 4; i++) {
                 var level = new Level();
 
-                for (int j = 0; j < 10; j++) {
+                int units = _random.Next(2, 10);
+                for (int j = 0; j < units; j++) {
                     level.Entities.Add(new Entity {
-                        Position = new Vector2(random.Next(0, 800), random.Next(0, 600)),
+                        Position = new Vector2(_random.Next(0, 800), _random.Next(0, 600)),
                         Texture = _unitTexture,
                         Origin = new Vector2(_unitTexture.Width / 2, _unitTexture.Height),
-                        Scale = new Vector2(0.4f),
+                        Scale = new Vector2(0.75f),
                     });
                 }
 
@@ -50,10 +52,16 @@ namespace LD50.Screens {
         }
 
         public void Update(GameTime gameTime) {
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
             for (int i = 0; i < 4; i++) {
                 if (_bindings.JustPressed(BindingId.Level1 + i)) {
                     _currentLevel = _levels[i];
                 }
+            }
+
+            for (int i = 0; i < _levels.Count; i++) {
+                UpdateLevel(_levels[i], deltaTime);
             }
         }
 
@@ -67,10 +75,41 @@ namespace LD50.Screens {
             for (int i = 0; i < _levels.Count; i++) {
                 Level level = _levels[i];
 
-                _spriteBatch.DrawString(_font, level.Name, new Vector2(8f + 160f * i, 8f), Color.Black);
+                _spriteBatch.DrawString(_font, level.Name, new Vector2(8f + 160f * i, 8f), level == _currentLevel ? Color.White : Color.Black);
             }
 
             _spriteBatch.End();
+        }
+
+        private void UpdateLevel(Level level, float deltaTime) {
+            for (int i = 0; i < level.Entities.Count; i++) {
+                Entity entity = level.Entities[i];
+
+                if (_random.Next(1000) == 0) {
+                    entity.TargetPosition = new Vector2(_random.Next(0, 800), _random.Next(0, 600));
+                }
+
+                if (entity.TargetPosition.HasValue) {
+                    float distance = Vector2.Distance(entity.Position, entity.TargetPosition.Value);
+                    float speed = 100f * deltaTime;
+                    if (distance < speed) {
+                        entity.Position = entity.TargetPosition.Value;
+                        entity.TargetPosition = null;
+                    }
+                    else {
+                        entity.Position += (entity.TargetPosition.Value - entity.Position) * (speed / distance);
+
+                        if (entity.TargetPosition.Value.X > entity.Position.X) {
+                            entity.Effects = SpriteEffects.None;
+                        }
+                        else {
+                            entity.Effects = SpriteEffects.FlipHorizontally;
+                        }
+
+                        entity.HopTimer += 15f * deltaTime;
+                    }
+                }
+            }
         }
 
         private void DrawEntity(Entity entity) {
@@ -80,13 +119,13 @@ namespace LD50.Screens {
 
             _spriteBatch.Draw(
                 entity.Texture,
-                entity.Position,
+                entity.Position + new Vector2(0f, -(float)Math.Abs(Math.Sin(entity.HopTimer)) * 10f),
                 null,
                 Color.White,
                 0f,
                 entity.Origin,
                 entity.Scale,
-                SpriteEffects.None,
+                entity.Effects,
                 0f);
         }
     }
